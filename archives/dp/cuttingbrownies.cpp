e4c9383a7ca6b8808909f52d772ce47fc9913caf
Rishi Sankar <rishi.sankar@gmail.com>
2021-02-03 02:02:29 -0800

    brownies

--- archives/dp/cuttingbrownies.cpp
+++ archives/dp/cuttingbrownies.cpp
@@ -1,48 +1,70 @@
-// doesn't work!!
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int S(int n, int m, vector<vector<int>>& memo) {
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define FOR(i, j, k) for (int i=j ; i<k ; ++i)
+#define FORI(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k) for (int i=j ; i>=k ; --i)
+#define RFORI(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j)
+#define RREP(i, j) RFOR(i, j, 0)
+#define OREP(i, j) FOR(i, 1, j+1)
+#define all(cont) cont.begin(), cont.end()
 
-    if (memo[n][m] != INT_MAX) {
-        return memo[n][m];
-    }
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
 
-    if (m == 1) {
-        return memo[n][m] = n-1;
-    } 
-
-    for (int k = 1; k < m; ++k) {
-        memo[n][m] = min(memo[n][m], -S(k,n,memo)-S(m-k,n,memo));
-    }
-
-    return memo[n][m];
-
-}
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> VI;
+typedef vector<vector<int>> VVI;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // cout.setf(ios::fixed);
+    // cout.precision(10);
 
-    int N; cin >> N;
-
-    for (int tests = 0; tests < N; ++tests) {
-
-        int m, n;
-        cin >> m >> n;
-        string person; cin >> person;
-        if (person == "Harry") {
-            int tmp = n;
-            n = m;
-            m = tmp;
+    vector<VVB> wins(501, VVB(501, VB(2, false)));
+    OREP(i, 500) {
+        OREP(j, 500) {
+            if (i == j) continue;
+            else if (j < i) {
+                wins[i][j][0] = wins[j][i][1];
+                wins[i][j][1] = wins[j][i][0];
+            } else {
+                if (i == 1) {
+                    wins[i][j][0] = true;
+                } else if (wins[i][j-1][0]) {
+                    wins[i][j][0] = true;
+                } else {
+                    for (int k = 1; k < j; ++k) {
+                        if ((!wins[i][k][1]) && (!wins[i][j-k][1])) {
+                            wins[i][j][0] = true;
+                            break;
+                        }
+                    }
+                }
+            }
         }
-        int bound = max(n,m) + 1;
-        vector<vector<int>> memo(bound, vector<int>(bound, INT_MAX));
+    }
 
-        cout << person;
-        if (S(n, m, memo) >= 0) {
-            cout << " cannot win\n";
-        } else {
+    int N;
+    cin >> N;
+
+    REP(i,N) {
+        int b,a; cin >> b >> a;
+        string s; cin >> s;
+        int index = 0;
+        if (s == "Harry") index = 1;
+        cout << s;
+        if (wins[a][b][index]) {
             cout << " can win\n";
+        } else {
+            cout << " cannot win\n";
         }
 
     }
