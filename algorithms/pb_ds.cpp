769f3c0c3eecf02b2c5bcd4afb36dde9fd8063c5
Rishi Sankar <rishi.sankar@gmail.com>
2021-12-13 19:13:37 -0800

    fft edu problem

--- algorithms/pb_ds.cpp
+++ algorithms/pb_ds.cpp
@@ -5,7 +5,8 @@ typedef pair<int,int> T;  // or any other comparable type
 
 tree < T ,  null_type ,  less<T> ,  rb_tree_tag ,  tree_order_statistics_node_update > pbds_tree;
 
-int ord = pbds_tree.order_of_key({qu.l, -1});  // finds index of key
-pbds_tree.find_by_order(ord);  // returns ptr/iter
+int ord = pbds_tree.order_of_key(key);  // ord = number of items strictly smaller than key
+pbds_tree.find_by_order(ord);  // returns ptr/iter to ord-th element in set.
 
 // plus all normal ordered_set functionality!
+// source: https://www.geeksforgeeks.org/ordered-set-gnu-c-pbds/
---
+++ codeforces/edu-118/F.cpp
@@ -0,0 +1,174 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PI 3.1415926535897932384626433832795
+#define MOD 998244353 
+#define FOR(i, j, k) for (int i=j ; i<k ; ++i)
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define FORI(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k) for (int i=j ; i>=k ; --i)
+#define RFORI(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j)
+#define RREP(i, j) RFOR(i, j, 0)
+#define OREP(i, j) FOR(i, 1, j+1)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define PB push_back
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+template<typename T> inline void printV(vector<T> &vec) {
+    cerr << "########\n";
+    for (int i = 0; i < vec.size(); ++i) {
+        cerr << vec[i] << ' ';
+    }
+    cerr << '\n';
+    cerr << "########\n";
+}
+template<typename T> inline void printVV(vector<vector<T>> &vec) {
+    cerr << "########\n";
+    for (int i = 0; i < vec.size(); ++i) {
+        for (int j = 0; j < vec[i].size(); ++j) {
+            cerr << vec[i][j] << ' ';
+        }
+        cerr << '\n';
+    }
+    cerr << "########\n";
+}
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+template<typename T> inline void dbg(T obj) { cerr << obj << '\n'; }
+template<typename T, typename... Args> inline void dbg(T t, Args... args) { cerr << t << " "; dbg(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+const ll mod = (119 << 23) + 1, root = 62; // = 998244353
+
+ll modpow(ll b, ll e) {
+	ll ans = 1;
+	for (; e; b = b * b % mod, e /= 2)
+		if (e & 1) ans = ans * b % mod;
+	return ans;
+}
+// For p < 2^30 there is also e.g. 5 << 25, 7 << 26, 479 << 21
+// and 483 << 21 (same root). The last two are > 10^9.
+typedef vector<ll> vl;
+void ntt(vl &a) {
+	int n = sz(a), L = 31 - __builtin_clz(n);
+	static vl rt(2, 1);
+	for (static int k = 2, s = 2; k < n; k *= 2, s++) {
+		rt.resize(n);
+		ll z[] = {1, modpow(root, mod >> s)};
+		rep(i,k,2*k) rt[i] = rt[i / 2] * z[i & 1] % mod;
+	}
+	vi rev(n);
+	rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+	rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+	for (int k = 1; k < n; k *= 2)
+		for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
+			ll z = rt[j + k] * a[i + j + k] % mod, &ai = a[i + j];
+			a[i + j + k] = ai - z + (z > ai ? mod : 0);
+			ai += (ai + z >= mod ? z - mod : z);
+		}
+}
+vl conv(const vl &a, const vl &b) {
+	if (a.empty() || b.empty()) return {};
+	int s = sz(a) + sz(b) - 1, B = 32 - __builtin_clz(s), n = 1 << B;
+	int inv = modpow(n, mod - 2);
+	vl L(a), R(b), out(n);
+	L.resize(n), R.resize(n);
+	ntt(L), ntt(R);
+	rep(i,0,n) out[-i & (n - 1)] = (ll)L[i] * R[i] % mod * inv % mod;
+	ntt(out);
+	return {out.begin(), out.begin() + s};
+}
+
+VVLL polys;
+
+void mult(int l, int r) {
+    if (l >= r) return;
+    int mid = (l+r)/2;
+    mult(l, mid);
+    mult(mid+1, r);
+    vl res = conv(polys[l], polys[mid+1]);
+    polys[l] = res;
+}
+
+void run() {
+    int n;
+    cin >> n;
+    VVI adjold(n);
+    REP(i, n-1) {
+        int a,b;
+        cin >> a >> b;
+        a--; b--;
+        adjold[a].PB(b);
+        adjold[b].PB(a);
+    }
+
+    stack<int> s;
+    s.push(0);
+    VVI adj(n);
+    VB visited(n, false);
+    visited[0] = true;
+    while (!s.empty()) {
+        int t = s.top();
+        s.pop();
+        for (int a : adjold[t]) {
+            if (visited[a]) continue;
+            adj[t].PB(a);
+            s.push(a);
+            visited[a] = true;
+        }
+    }
+
+    VLL fact(n+1);
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+        fact[i] = (fact[i-1] * i) % MOD;
+    }
+
+    REP(i, n) {
+        polys.PB({1, (ll)adj[i].size()});
+    }
+    mult(0, n-1);
+
+    ll sm = 0;
+    ll v = -1;
+    for (int k = 0; k < n; ++k) {
+        v *= -1;
+        sm += v * polys[0][k] * fact[n-k];
+        sm %= MOD;
+    }
+    print((sm+MOD)%MOD);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(10);
+    // ll t; cin >> t;
+    ll t=1;
+    REP(tests,t) run();
+}
