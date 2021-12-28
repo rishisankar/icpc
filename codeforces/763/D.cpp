2d4326af8fdd95df0dc4ff60d47bb00a068f0d6c
Rishi Sankar <rishi.sankar@gmail.com>
2021-12-28 11:48:26 -0800

    div 2 round 763

---
+++ codeforces/763/D.cpp
@@ -0,0 +1,280 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PI 3.1415926535897932384626433832795
+#define FOR(i, j, k) for (int i=j ; i<k ; ++i)
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define FORI(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k) for (int i=j ; i>=k ; --i)
+#define RFORI(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j)
+#define RREP(i, j) RFOR(i, j, 0)
+#define OREP(i, j) FOR(i, 1, j+1)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define PB push_back
+#define F first
+#define S second
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+template<typename T> inline void printV(vector<T> &vec) {
+    cerr << "########\n";
+    for (int i = 0; i < vec.size(); ++i) {
+        cerr << vec[i] << ' ';
+    }
+    cerr << '\n';
+    cerr << "########\n";
+}
+template<typename T> inline void printVV(vector<vector<T>> &vec) {
+    cerr << "########\n";
+    for (int i = 0; i < vec.size(); ++i) {
+        for (int j = 0; j < vec[i].size(); ++j) {
+            cerr << vec[i][j] << ' ';
+        }
+        cerr << '\n';
+    }
+    cerr << "########\n";
+}
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+template<typename T> inline void dbg(T obj) { cerr << obj << '\n'; }
+template<typename T, typename... Args> inline void dbg(T t, Args... args) { cerr << t << " "; dbg(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+
+template<const int &MOD>
+struct _m_int {
+    int val;
+
+    _m_int(int64_t v = 0) {
+        if (v < 0) v = v % MOD + MOD;
+        if (v >= MOD) v %= MOD;
+        val = int(v);
+    }
+
+    _m_int(uint64_t v) {
+        if (v >= MOD) v %= MOD;
+        val = int(v);
+    }
+
+    _m_int(int v) : _m_int(int64_t(v)) {}
+    _m_int(unsigned v) : _m_int(uint64_t(v)) {}
+
+    explicit operator int() const { return val; }
+    explicit operator unsigned() const { return val; }
+    explicit operator int64_t() const { return val; }
+    explicit operator uint64_t() const { return val; }
+    explicit operator double() const { return val; }
+    explicit operator long double() const { return val; }
+
+    _m_int& operator+=(const _m_int &other) {
+        val -= MOD - other.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+
+    _m_int& operator-=(const _m_int &other) {
+        val -= other.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
+#if !defined(_WIN32) || defined(_WIN64)
+        return unsigned(x % m);
+#endif
+        // Optimized mod for Codeforces 32-bit machines.
+        // x must be less than 2^32 * m for this to work, so that x / m fits in an unsigned 32-bit int.
+        unsigned x_high = unsigned(x >> 32), x_low = unsigned(x);
+        unsigned quot, rem;
+        asm("divl %4\n"
+            : "=a" (quot), "=d" (rem)
+            : "d" (x_high), "a" (x_low), "r" (m));
+        return rem;
+    }
+
+    _m_int& operator*=(const _m_int &other) {
+        val = fast_mod(uint64_t(val) * other.val);
+        return *this;
+    }
+
+    _m_int& operator/=(const _m_int &other) {
+        return *this *= other.inv();
+    }
+
+    friend _m_int operator+(const _m_int &a, const _m_int &b) { return _m_int(a) += b; }
+    friend _m_int operator-(const _m_int &a, const _m_int &b) { return _m_int(a) -= b; }
+    friend _m_int operator*(const _m_int &a, const _m_int &b) { return _m_int(a) *= b; }
+    friend _m_int operator/(const _m_int &a, const _m_int &b) { return _m_int(a) /= b; }
+
+    _m_int& operator++() {
+        val = val == MOD - 1 ? 0 : val + 1;
+        return *this;
+    }
+
+    _m_int& operator--() {
+        val = val == 0 ? MOD - 1 : val - 1;
+        return *this;
+    }
+
+    _m_int operator++(int) { _m_int before = *this; ++*this; return before; }
+    _m_int operator--(int) { _m_int before = *this; --*this; return before; }
+
+    _m_int operator-() const {
+        return val == 0 ? 0 : MOD - val;
+    }
+
+    friend bool operator==(const _m_int &a, const _m_int &b) { return a.val == b.val; }
+    friend bool operator!=(const _m_int &a, const _m_int &b) { return a.val != b.val; }
+    friend bool operator<(const _m_int &a, const _m_int &b) { return a.val < b.val; }
+    friend bool operator>(const _m_int &a, const _m_int &b) { return a.val > b.val; }
+    friend bool operator<=(const _m_int &a, const _m_int &b) { return a.val <= b.val; }
+    friend bool operator>=(const _m_int &a, const _m_int &b) { return a.val >= b.val; }
+
+    static const int SAVE_INV = int(1e6) + 5;
+    static _m_int save_inv[SAVE_INV];
+
+    static void prepare_inv() {
+        // Ensures that MOD is prime, which is necessary for the inverse algorithm below.
+        for (int64_t p = 2; p * p <= MOD; p += p % 2 + 1)
+            assert(MOD % p != 0);
+
+        save_inv[0] = 0;
+        save_inv[1] = 1;
+
+        for (int i = 2; i < SAVE_INV; i++)
+            save_inv[i] = save_inv[MOD % i] * (MOD - MOD / i);
+    }
+
+    _m_int inv() const {
+        if (save_inv[1] == 0)
+            prepare_inv();
+
+        if (val < SAVE_INV)
+            return save_inv[val];
+
+        _m_int product = 1;
+        int v = val;
+
+        while (v >= SAVE_INV) {
+            product *= MOD - MOD / v;
+            v = MOD % v;
+        }
+
+        return product * save_inv[v];
+    }
+
+    _m_int pow(int64_t p) const {
+        if (p < 0)
+            return inv().pow(-p);
+
+        _m_int a = *this, result = 1;
+
+        while (p > 0) {
+            if (p & 1)
+                result *= a;
+
+            p >>= 1;
+
+            if (p > 0)
+                a *= a;
+        }
+
+        return result;
+    }
+
+    friend ostream& operator<<(ostream &os, const _m_int &m) {
+        return os << m.val;
+    }
+};
+
+template<const int &MOD> _m_int<MOD> _m_int<MOD>::save_inv[_m_int<MOD>::SAVE_INV];
+
+// extern const int MOD = 998244353;
+extern const int MOD = int(1e9) + 7;
+using mod_int = _m_int<MOD>;
+
+
+void run() {
+    ll n,m,rb,cb,rd,cd, P;
+    cin >> n >> m >> rb >> cb >> rd >> cd >> P;
+    vector<ll> t;
+    int dr = 1; int dc = 1;
+    if (rb == n) dr = -1;
+    if (rb == 1) dr = 1;
+    if (cb == m) dc = -1;
+    if (cb == 1) dc = 1;
+    int ogdr = dr, ogdc = dc;
+    int ogr = rb; int ogc = cb;
+    ll ct = 0;
+    while (true) {
+        if (rb == rd || cb == cd) {
+            t.PB(ct);
+        }
+        ++ct;
+        rb += dr;
+        cb += dc;
+        if (rb == n) dr = -1;
+        if (rb == 1) dr = 1;
+        if (cb == m) dc = -1;
+        if (cb == 1) dc = 1;
+        if (rb == ogr && cb == ogc && dr == ogdr && dc == ogdc) break;
+    }
+
+    int k = t.size();
+    mod_int p = P;
+    mod_int omp = 100; omp -= p;
+    mod_int pomk = 1;
+    REP(i,k) pomk *= omp;
+
+    vector<mod_int> p100(k+1);
+    p100[0] = 1;
+    OREP(i,k) p100[i] = p100[i-1] * 100;
+
+    mod_int x1 = 0;
+    mod_int pwr = 1;
+    REP(i, k) {
+        mod_int tmp = p * t[i] * pwr;
+        tmp *= p100[k-1-i];
+        x1 += tmp;
+        pwr *= omp;
+    }
+    mod_int y1 = p100[k];
+
+    mod_int x2 = p100[k] * x1 + pomk * y1 * ct;
+    mod_int y2 = p100[k] * y1 - pomk * y1;
+    mod_int res = x2 / y2;
+    print(res);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(10);
+    ll t; cin >> t;
+    // ll t=1;
+    REP(tests,t) run();
+}
