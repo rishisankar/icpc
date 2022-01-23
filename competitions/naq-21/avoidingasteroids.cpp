4d9a7e6c18ff4fef492176583223bffbf6fa193d
Rishi Sankar <rishi.sankar@gmail.com>
2022-01-22 20:55:10 -0800

    avoiding asteroids

--- competitions/naq-21/avoidingasteroids.cpp
+++ competitions/naq-21/avoidingasteroids.cpp
@@ -63,134 +63,66 @@ typedef pair<int, int> pii;
 typedef vector<pair<int, int>> VPII;
 typedef vector<vector<pair<int, int>>> VVPII;
 
-template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
-template<class T>
-struct Point {
-	typedef Point P;
-	T x, y;
-	explicit Point(T x=0, T y=0) : x(x), y(y) {}
-	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
-	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
-	P operator+(P p) const { return P(x+p.x, y+p.y); }
-	P operator-(P p) const { return P(x-p.x, y-p.y); }
-	P operator*(T d) const { return P(x*d, y*d); }
-	P operator/(T d) const { return P(x/d, y/d); }
-	T dot(P p) const { return x*p.x + y*p.y; }
-	T cross(P p) const { return x*p.y - y*p.x; }
-	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
-	T dist2() const { return x*x + y*y; }
-	double dist() const { return sqrt((double)dist2()); }
-	// angle to x-axis in interval [-pi, pi]
-	double angle() const { return atan2(y, x); }
-	P unit() const { return *this/dist(); } // makes dist()=1
-	P perp() const { return P(-y, x); } // rotates +90 degrees
-	P normal() const { return perp().unit(); }
-	// returns point rotated 'a' radians ccw around the origin
-	P rotate(double a) const {
-		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
-	friend ostream& operator<<(ostream& os, P p) {
-		return os << "(" << p.x << "," << p.y << ")"; }
-};
+double x1,y11,z1,x2,y2,z2; // endpts of line segment
+double x,y,z,vx,vy,vz,r; // starting point, direction, radius of ray
 
-template<class P>
-vector<P> circleLine(P c, double r, P a, P b) {
-	P ab = b - a, p = a + ab * (c-a).dot(ab) / ab.dist2();
-	double s = a.cross(b, c), h2 = r*r - s*s / ab.dist2();
-	if (h2 < 0) return {};
-	if (h2 == 0) return {p};
-	P h = ab.unit() * sqrt(h2);
-	return {p - h, p + h};
-}
+// x between 0 and 1
+double f(double i) {
+    double nx = x1 + i*(x2-x1);
+    double ny = y11 + i*(y2-y11);
+    double nz = z1 + i*(z2-z1);
 
-bool checkInCyl(ld r, ld x, ld y, ld z) {
-    ld dist = sqrt(x*x+y*y);
-    return dist <= r && z >= 0;
-}
+    // projection of point on line: A + dot(AP,AB) / dot(AB,AB) * AB
+    double dAB = vx*vx+vy*vy+vz*vz;
+    double dAP = (nx-x)*vx + (ny-y)*vy + (nz-z)*vz;
+    double px = x + dAP/dAB*vx;
+    double py = y + dAP/dAB*vy;
+    double pz = z + dAP/dAB*vz;
+    double dt = (x-px)*vx + (y-py)*vy + (z-pz)*vz;
 
-ld getZ(ld x1,ld y1,ld z1, ld x2,ld y2,ld z2, ld a, ld b) {
-    if (y2 == y1) return z1 + (z2-z1) * (a - x1) / (x2 - x1);
-    else return z1 + (z2-z1) * (b - y1) / (y2 - y1);
-}
-
-bool onSameSide(ld x1,ld y1,ld z1, ld x2,ld y2,ld z2, ld a, ld b) {
-    ld vx1 = x2-x1;
-    ld vy1 = y2-y1;
-    ld vz1 = z2-z1;
-    ld c = getZ(x1,y1,z1,x2,y2,z2,a,b);
-    ld vx2 = a-x1;
-    ld vy2 = b-y1;
-    ld vz2 = c-z1;
-    ld dot = vx1*vx2+vy1*vy2+vz1*vz2;
-    return dot > 0;
-}
-
-bool inSegment(ld x1,ld y1,ld z1, ld x2,ld y2,ld z2, ld a, ld b) {
-    return onSameSide(x1,y1,z1,x2,y2,z2,a,b) && onSameSide(x2,y2,z2,x1,y1,z1,a,b);
-}
-
-bool checkIntersection(ld sx, ld sy, ld sz, ld bx, ld by, ld bz, ld r) {
-    dbg("checking intersection", sx,sy,sz,bx,by,bz,r);
-    Point<ld> c(0,0);
-    Point<ld> a(sx,sy);
-    Point<ld> b(bx,by);
-
-    if (checkInCyl(r,sx,sy,sz) || checkInCyl(r,bx,by,bz)) return true;
-
-    if (a.x == b.x && a.y == by) return false;
-
-    vector<Point<ld>> intersect = circleLine(c, r, a, b);
-
-    if (intersect.size() == 0) {
-        return false;
-    } else if (intersect.size() == 1) {
-        ld x = intersect[0].x;
-        ld y = intersect[0].y;
-        return (inSegment(sx,sy,sz,bx,by,bz,x,y) && getZ(sx,sy,sz,bx,by,bz,x,y) >= 0);
+    // dbg("pt on line seg",nx,ny,nz);
+    // dbg("ray center",x,y,z);
+    // dbg("AB",vx,vy,vz);
+    // dbg("dot prods",dAB,dAP);
+    // dbg("proj",px,py,pz);
+    // dbg("vector dp",dt);
+    if (dt > 0) {
+        // projection not on ray --> take distance to endpt of ray
+        return -sqrt((nx-x)*(nx-x) + (ny-y)*(ny-y) + (nz-z)*(nz-z));
     } else {
-        ld x0 = intersect[0].x;
-        ld y0 = intersect[0].y;
-        ld x1 = intersect[1].x;
-        ld y1 = intersect[1].y;
-        dbg(x0,y0,x1,y1);
-        dbg(getZ(sx,sy,sz,bx,by,bz,x0,y0));
-        dbg(getZ(sx,sy,sz,bx,by,bz,x1,y1));
-        return ( (inSegment(sx,sy,sz,bx,by,bz,x0,y0) && getZ(sx,sy,sz,bx,by,bz,x0,y0) >= 0) ||
-            (inSegment(sx,sy,sz,bx,by,bz,x1,y1) && getZ(sx,sy,sz,bx,by,bz,x1,y1) >= 0)  );
+        // projection on ray --> take distance to projection
+        return -sqrt((nx-px)*(nx-px) + (ny-py)*(ny-py) + (nz-pz)*(nz-pz));
     }
 }
 
-// void run() {
-//     ld sx, sy, sz, bx, by, bz, r;
-//     cin >> sx >> sy >> sz >> bx >> by >> bz >> r;
-//     cout << (checkIntersection(sx,sy,sz,bx,by,bz,r) ? "intersect" : "no intersect") << '\n';
-// }
-
-
-array<ld, 3> compute(ld matrix[3][3], array<ld, 3> v) {
-	array<ld, 3> out { 0, 0, 0 };
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			out[i] += matrix[i][j] * v[j];
-		}
-	}
-	return out;
+double ternary_search(double l, double r) {
+    double eps = 1e-9;              //set the error limit here
+    while (r - l > eps) {
+        double m1 = l + (r - l) / 3;
+        double m2 = r - (r - l) / 3;
+        double f1 = f(m1);      //evaluates the function at m1
+        double f2 = f(m2);      //evaluates the function at m2
+        if (f1 < f2)
+            l = m1;
+        else
+            r = m2;
+    }
+    return -f(l);                    //return the maximum of f(x) in [l, r]
 }
 
 void run() {
-	ld sx, sy, sz;
-	cin >> sx >> sy >> sz;
-	ld bx, by, bz;
-	cin >> bx >> by >> bz;
+	cin >> x1 >> y11 >> z1;
+	cin >> x2 >> y2 >> z2;
 	int n;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		ld px, py, pz;
+		double px, py, pz;
 		cin >> px >> py >> pz;
-		ld dx, dy, dz;
+		double dx, dy, dz;
 		cin >> dx >> dy >> dz;
 		int m;
 		cin >> m;
-		ld dist = 0;
+		double dist = 0;
 		for (int j = 0; j < m; j++) {
 			double cx, cy, cz;
 			cin >> cx >> cy >> cz;
@@ -198,32 +130,16 @@ void run() {
 					(cx - px) * (cx - px) + (cy - py) * (cy - py)
 							+ (cz - pz) * (cz - pz));
 		}
-		dist = sqrt(dist);
-		array<ld, 3> eP { dx, dy, dz };
-		ld theta = -atan2(dx, dy);
-		ld matrix[3][3] { { cos(theta), -sin(theta), 0 }, { sin(theta), cos(
-				theta), 0 }, { 0, 0, 1 } };
-		array<ld, 3> tV = compute(matrix, eP);
-		ld theta1 = -atan2(tV[1], tV[2]);
-		ld matrix2[3][3] { { 1, 0, 0 }, { 0, cos(theta1), -sin(theta1) }, { 0,
-				sin(theta1), cos(theta1) } };
-		ld cM[3][3] { };
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				for (int k = 0; k < 3; k++) {
-					cM[i][k] += matrix[i][j] * matrix2[j][k];
-				}
-			}
-		}
-		array<ld,3> mStart=compute(cM,{sx-px,sy-py,sz-pz});
-		array<ld,3> mBase= compute(cM,{bx-px,by-py,bz-pz});
+        x=px;y=py;z=pz;
+        vx=dx;vy=dy;vz=dz;
+		r = sqrt(dist);
 
-        if (checkIntersection(mStart[0],mStart[1],mStart[2], mBase[0], mBase[1], mBase[2], dist)) {
+        double opt = ternary_search(0, 1);
+        if (opt < r) {
             print("Surrender");
             return;
-        } else {
-            dbg("Go");
         }
+
 	}
     print("Go");
 }
