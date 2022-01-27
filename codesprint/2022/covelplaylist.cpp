732eff94c0cb67b9d3afdb3848a795f47edac795
Rishi Sankar <rishi.sankar@gmail.com>
2022-01-27 15:22:00 -0800

    covelplaylist

--- codesprint/2022/covelplaylist.cpp
+++ codesprint/2022/covelplaylist.cpp
@@ -62,8 +62,98 @@ typedef pair<int, int> pii;
 typedef vector<pair<int, int>> VPII;
 typedef vector<vector<pair<int, int>>> VVPII;
 
+const int MOD = int(1e9) + 9;
+vector<string> st;
+int n, m;
+
+const ll P1 = 11;
+const ll P2 = 13;
+
+void chshP(int k, int sti, vector<ll>& hsh, ll P) {
+    unordered_map<ll,ll> invv; invv[11] = 363636367; invv[13] = 615384621;
+    ll h = 0;
+    ll pw = 1;
+    REP(i, k-1) {
+        h += (st[sti][i]-'A'+1) * pw;
+        h%=MOD;
+        pw *= P;
+        pw%=MOD;
+    }
+    REP(i, n) {
+        char vl = (i+k-1 >= n) ? 'H' : st[sti][i+k-1];
+        h += (vl-'A'+1) * pw;
+        h%= MOD;
+        hsh[i] = h;
+        h -= (st[sti][i]-'A'+1);
+        h = (h+MOD)%MOD;
+        h *= invv[P];
+        h %= MOD;
+    }
+}
+
+void chsh(int k, int sti, vector<ll>& hsh) {
+    VLL v1(n), v2(n);
+    chshP(k,sti,v1,P1); chshP(k,sti,v2,P2);
+    REP(i, n) {
+        hsh[i] = (v1[i] << 32) + v2[i];
+    }
+}
+
+bool match(int i, int j, int si, int k) {
+    REP(a, k) {
+        char v1 = (i+a >= n) ? 'H' : st[0][i+a];
+        char v2 = (j+a >= n) ? 'H' : st[si][j+a];
+        if (v1 != v2) return false;
+    }
+    return true;
+}
+
+bool check(int k) {
+    unordered_map<ll, unordered_set<ll>> mp;
+    vector<ll> h1(n); chsh(k,0,h1);
+    // printV(h1);
+    int ct = 0;
+    REP(i,n) {
+        mp[h1[i]].insert(i);
+    }
+    for (int i = 1; i < m; ++i) {
+        vector<ll> hv(n); chsh(k,i,hv);
+        // printV(hv);
+        REP(j,n) {
+            if (mp.count(hv[j]) && mp[hv[j]].size()>0) {
+                for (int ind : mp[hv[j]]) {
+                    if (match(ind, j, i, k)) {
+                        // dbg("match", ind, j, i, k);
+                        mp[hv[j]].erase(ind);
+                        ++ct;
+                    }
+                }
+            }
+        }
+    }
+    return ct*2 <= n;
+}
+
 void run() {
-    // int n; cin >> n ; VLL v(n); INP(v,n);
+    cin >> n >> m;
+    st.resize(m);
+    REP(i,m) {
+        cin >> st[i];
+    }
+    int lo = 1; int hi = n;
+    int best = -1;
+    while (lo <= hi) {
+        int mid = (lo+hi)/2;
+        bool t = check(mid);
+        // dbg("check",mid,t);
+        if (t) {
+            best = mid;
+            hi = mid - 1;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    print(best);
 }
 
 int main() {
