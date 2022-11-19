d74bcaf1e7ab25d6fd239ca317a1ffbe4e8790a1
Rishi Sankar <rishi.sankar@gmail.com>
2022-11-19 01:54:16 -0800

    graphs problems

---
+++ kattis/graphs/appealtotheaudience.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, j, k) for (int i=j ; i<k ; ++i)
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define FORI(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k) for (int i=j ; i>=k ; --i)
+#define RFORI(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j)
+#define RREP(i, j) RFOR(i, j, 0)
+#define OREP(i, j) FOR(i, 1, j+1)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define PB push_back
+#define F first
+#define S second
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+template<typename T> inline void printV(vector<T> &vec) {
+    cerr << "########\n";
+    for (int i = 0; i < vec.size(); ++i) {
+        cerr << vec[i] << ' ';
+    }
+    cerr << '\n';
+    cerr << "########\n";
+}
+template<typename T> inline void printVV(vector<vector<T>> &vec) {
+    cerr << "########\n";
+    for (int i = 0; i < vec.size(); ++i) {
+        for (int j = 0; j < vec[i].size(); ++j) {
+            cerr << vec[i][j] << ' ';
+        }
+        cerr << '\n';
+    }
+    cerr << "########\n";
+}
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+template<typename T> inline void dbg(T obj) { cerr << obj << '\n'; }
+template<typename T, typename... Args> inline void dbg(T t, Args... args) { cerr << t << " "; dbg(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+const ld pi = 3.1415926535897932384626433832795;
+const ll mod = 1000000007;
+
+VI depth;
+VB valid;
+int solve(int x, VVI& adj) {
+    if (depth[x] != -1) return depth[x];
+    if (adj[x].size() == 0) return depth[x] = 1;
+    int bst = 0;
+    int bsti = 0;
+    for (int i = 0; i < adj[x].size(); ++i) {
+        int c = adj[x][i];
+        if (solve(c,adj) > bst) {
+            bst = solve(c,adj);
+            bsti = c;
+        }
+    }
+    valid[bsti] = false;
+    return depth[x] = 1 + bst;
+}
+void run() {
+    int n,k;
+    cin >> n >> k;
+    vector<int> p(n);
+    VLL pw(k);
+    INP(pw,k);
+    VVI adj(n);
+    for (int i = 1; i < n; ++i) {
+        cin >> p[i];
+        adj[p[i]].PB(i);
+    }
+    depth.assign(n,-1);
+    valid.assign(n, true);
+    solve(0, adj);
+    VLL vals;
+    REP(i,n) {
+        if (valid[i]) vals.PB(depth[i]);
+    }
+    sort(all(pw));
+    sort(all(vals));
+    ll res = 0;
+    REP(i,k) {
+        res += pw[i] * vals[i];
+    }
+    print(res-pw[pw.size()-1]);
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(15);
+    // ll t; cin >> t;
+    ll t=1;
+    REP(tests,t) run();
+}
---
+++ kattis/graphs/thekingofthenorth.cpp
@@ -0,0 +1,175 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, j, k) for (int i=j ; i<k ; ++i)
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define FORI(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k) for (int i=j ; i>=k ; --i)
+#define RFORI(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j)
+#define RREP(i, j) RFOR(i, j, 0)
+#define OREP(i, j) FOR(i, 1, j+1)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define PB push_back
+#define F first
+#define S second
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+template<typename T> inline void printV(vector<T> &vec) {
+    cerr << "########\n";
+    for (int i = 0; i < vec.size(); ++i) {
+        cerr << vec[i] << ' ';
+    }
+    cerr << '\n';
+    cerr << "########\n";
+}
+template<typename T> inline void printVV(vector<vector<T>> &vec) {
+    cerr << "########\n";
+    for (int i = 0; i < vec.size(); ++i) {
+        for (int j = 0; j < vec[i].size(); ++j) {
+            cerr << vec[i][j] << ' ';
+        }
+        cerr << '\n';
+    }
+    cerr << "########\n";
+}
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+template<typename T> inline void dbg(T obj) { cerr << obj << '\n'; }
+template<typename T, typename... Args> inline void dbg(T t, Args... args) { cerr << t << " "; dbg(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+const ld pi = 3.1415926535897932384626433832795;
+const ll mod = 1000000007;
+
+/**
+ * Author: chilli
+ * Date: 2019-04-26
+ * License: CC0
+ * Source: https://cp-algorithms.com/graph/dinic.html
+ * Description: Flow algorithm with complexity $O(VE\log U)$ where $U = \max |\text{cap}|$.
+ * $O(\min(E^{1/2}, V^{2/3})E)$ if $U = 1$; $O(\sqrt{V}E)$ for bipartite matching.
+ * Status: Tested on SPOJ FASTFLOW and SPOJ MATCHING, stress-tested
+ */
+
+struct Dinic {
+	struct Edge {
+		int to, rev;
+		ll c, oc;
+		ll flow() { return max(oc - c, 0LL); } // if you need flows
+	};
+	vi lvl, ptr, q;
+	vector<vector<Edge>> adj;
+	Dinic(int n) : lvl(n), ptr(n), q(n), adj(n) {}
+	void addEdge(int a, int b, ll c, ll rcap = 0) {
+		adj[a].push_back({b, sz(adj[b]), c, c});
+		adj[b].push_back({a, sz(adj[a]) - 1, rcap, rcap});
+	}
+	ll dfs(int v, int t, ll f) {
+		if (v == t || !f) return f;
+		for (int& i = ptr[v]; i < sz(adj[v]); i++) {
+			Edge& e = adj[v][i];
+			if (lvl[e.to] == lvl[v] + 1)
+				if (ll p = dfs(e.to, t, min(f, e.c))) {
+					e.c -= p, adj[e.to][e.rev].c += p;
+					return p;
+				}
+		}
+		return 0;
+	}
+	ll calc(int s, int t) {
+		ll flow = 0; q[0] = s;
+		rep(L,0,31) do { // 'int L=30' maybe faster for random data
+			lvl = ptr = vi(sz(q));
+			int qi = 0, qe = lvl[s] = 1;
+			while (qi < qe && !lvl[t]) {
+				int v = q[qi++];
+				for (Edge e : adj[v])
+					if (!lvl[e.to] && e.c >> (30 - L))
+						q[qe++] = e.to, lvl[e.to] = lvl[v] + 1;
+			}
+			while (ll p = dfs(s, t, LLONG_MAX)) flow += p;
+		} while (lvl[t]);
+		return flow;
+	}
+	bool leftOfMinCut(int a) { return lvl[a] != 0; }
+};
+
+int r,c; 
+
+int getI(int x, int y, bool isleft) {
+    return 2*(x + y*r) + isleft;
+}
+
+void run() {
+    cin >> r >> c;
+    int N = r*c*2;
+    VVLL v(r, VLL(c));
+    REP(i, r) {
+        REP(j, c) {
+            cin >> v[i][j];
+        }
+    }
+    Dinic d(N+1);
+    int cr,cc; cin >> cr >> cc;
+    // source  = N
+    ll infty = 300LL * 300LL * 1000000LL;
+    vector<PII> dirs = {{-1,0}, {0,1}, {1,0}, {0,-1}};
+    REP(i, r) {
+        REP(j, c) {
+            int li = getI(i,j, true);
+            int ri = getI(i,j,false);
+            d.addEdge(li, ri, v[i][j]);
+            bool border = false;
+            for (PII dir : dirs) {
+                int x = i+dir.F;
+                int y = j + dir.S;
+                if (x < 0 || y < 0 || x >= r || y >= c) {
+                    // border point!
+                    border = true;
+                    continue;
+                } else {
+                    d.addEdge(ri, getI(x,y,true), v[i][j]);
+                }
+            }
+            if (border) {
+                d.addEdge(N, li, infty);
+            }
+        }
+    }
+    print(d.calc(N, getI(cr,cc,false)));
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(15);
+    // ll t; cin >> t;
+    ll t=1;
+    REP(tests,t) run();
+}
