60025c7df66979ec696b88c0872e25f71d5360b1
Rishi Sankar <rishi.sankar@gmail.com>
2022-11-20 12:10:28 -0800

    modular arithmetic training

--- kattis/numbertheory/threedigits.cpp
+++ kattis/numbertheory/threedigits.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define PI 3.1415926535897932384626433832795
-#define MOD 1000000007
 #define FOR(i, j, k) for (int i=j ; i<k ; ++i)
 #define rep(i, a, b) for(int i = a; i < (b); ++i) 
 #define FORI(i, j, k, in) for (int i=j ; i<k ; i+=in)
@@ -14,29 +12,37 @@ using namespace std;
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 #define PB push_back
+#define F first
+#define S second
 #define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
 
 template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
 template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
 
 template<typename T> inline void printV(vector<T> &vec) {
-    cout << "########\n";
+    cerr << "########\n";
     for (int i = 0; i < vec.size(); ++i) {
-        cout << vec[i] << ' ';
+        cerr << vec[i] << ' ';
     }
-    cout << '\n';
-    cout << "########\n";
+    cerr << '\n';
+    cerr << "########\n";
 }
 template<typename T> inline void printVV(vector<vector<T>> &vec) {
-    cout << "########\n";
+    cerr << "########\n";
     for (int i = 0; i < vec.size(); ++i) {
         for (int j = 0; j < vec[i].size(); ++j) {
-            cout << vec[i][j] << ' ';
+            cerr << vec[i][j] << ' ';
         }
-        cout << '\n';
+        cerr << '\n';
     }
-    cout << "########\n";
+    cerr << "########\n";
 }
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+template<typename T> inline void dbg(T obj) { cerr << obj << '\n'; }
+template<typename T, typename... Args> inline void dbg(T t, Args... args) { cerr << t << " "; dbg(args...); }
 
 typedef long long ll;
 typedef long double ld;
@@ -55,36 +61,148 @@ typedef pair<int, int> pii;
 typedef vector<pair<int, int>> VPII;
 typedef vector<vector<pair<int, int>>> VVPII;
 
+const ld pi = 3.1415926535897932384626433832795;
+const ll mod = 1000000007;
+
+// Returns largest power of p that divides n! 
+ll largestPower(ll n, ll p) { 
+    ll x = 0;
+    while (n) { 
+        n /= p;
+        x += n;
+    }
+    return x;
+}
+
+// O(log(min(a,b))), can use __gcd instead
+ll gcd(ll a, ll b) {
+    if (a == 0) return b; 
+    return gcd(b % a, a); 
+} 
+
+// note that lcm(i, j) = ij / gcd(i,j)
+ll lcm(ll a, ll b) {
+  return a*b/gcd(a,b);
+}
+
+// return gcd(a,b) and solve for integer x,y: ax+by=gcd(a,b)
+// linear diophantine equations ax+by=c have a solution iff c divides gcd(a,b)
+ll euclidean(ll a, ll b, ll &x, ll &y) {
+
+    if (a == 0) {
+        x = 0;
+        y = 1;
+        return b;
+    }
+
+    ll x1,y1;
+    ll gcd_ = euclidean(b%a, a, x1, y1);
+
+    x = y1 - (b/a) * x1;
+    y = x1;
+
+    return gcd_;
+
+}
+
+// only exists if gcd(a,m) = 1 !! otherwise return -1
+ll modularInverse(ll a, ll m) {
+    ll x,y;
+    ll g = euclidean(a, m, x, y);
+    if (g != 1) return -1;
+    return (x % m + m) % m; // shift x to positive in case it is negative
+}
+
+template <typename T>
+T modpow(T base, T exp, T modulus) {
+  base %= modulus;
+  T result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = (result * base) % modulus;
+    base = (base * base) % modulus;
+    exp >>= 1;
+  }
+  return result;
+}
+
+ll euclid(ll a, ll b, ll &x, ll &y) {
+	if (!b) return x = 1, y = 0, a;
+	ll d = euclid(b, a % b, y, x);
+	return y -= a/b * x, d;
+}
+
+/*
+
+CRT: find x s.t. x = a (mod m) and x = b (mod n)
+Return -1 if no solution
+Time: log(max(m,n))
+Source: kactl
+Example: numbertheory/generalchineseremainder
+Note: not necessarily the smallest nonnegative soln. crt(...) % lcm(m,n) is the smallest.
+*/
+ll crt(ll a, ll m, ll b, ll n) {
+	if (n > m) swap(a, b), swap(m, n);
+	ll x, y, g = euclid(m, n, x, y);
+	if ((a - b) % g == 0) {
+	    x = (b - a) % n * x % n / g * m + a;
+	    return x < 0 ? x + m*n/g : x;
+    } else {
+        return -1;
+    }
+}
+
+void run() {
+    ll n; cin >> n;
+    ll lp5 = largestPower(n, 5);
+    ll lp2 = largestPower(n, 2);
+    ll trz = min(lp5, lp2);
+    dbg(lp5,lp2);
+    ll p1 = 1;
+    ll p2 = 1;
+    ll rm = trz;
+    for (ll i = 1; i <= n; ++i) {
+        ll x = i;
+        while (rm > 0 && x % 5 == 0) {
+            x/=5;
+            --rm;
+        }
+        p1 *= x;
+        p1 %= 125;
+    }
+    rm = trz;
+    for (ll i = 1; i <= n; ++i) {
+        ll x = i;
+        while (rm > 0 && x % 2 == 0) {
+            x/=2;
+            --rm;
+        }
+        p2 *= x;
+        p2 %= 8;
+    }
+    ll ti = modularInverse(2, 125);
+    ll fi = modularInverse(5, 8);
+    REP(i, trz) {
+        p1 *= ti;
+        p1 %= 125;
+        p2 *= fi;
+        p2 %= 8;
+    }
+    dbg("mods", p1, p2);
+    ll ans = crt(p1, 125, p2, 8);
+    ans = ans % 1000;
+    if (n < 7) { print(ans); return; }
+    if (ans < 100) cout << "0";
+    if (ans < 10) cout << "0";
+    print(ans);
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin.exceptions(cin.failbit);
     // cout.setf(ios::fixed);
-    // cout.precision(10);
-
-    ll n; cin >> n;
-    ll x = 1;
-    int ct2 = 0, ct5 = 0;
-    for (int i = 1; i <= n; ++i) {
-        int tmp = i;
-        while (tmp % 2 == 0) {
-            tmp /= 2;
-            ++ct2;
-        }
-        while (tmp % 5 == 0) {
-            tmp /= 5;
-            ++ct5;
-        }
-        x *= tmp;
-        x %= 1000;
-    }
-    for (int i = 0; i < ct2-ct5; ++i) {
-        x = (x*2)%1000;
-    }
-    if (n >= 7) {
-        if (x < 100) cout << "0";
-        if (x < 10) cout << "0";
-    }
-    cout << x << '\n';
-
+    // cout.precision(15);
+    // ll t; cin >> t;
+    ll t=1;
+    REP(tests,t) run();
 }
