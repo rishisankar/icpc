d3eec0a4700649be3c8139ea044c225cb5d327df
Rishi Sankar <rishi.sankar@gmail.com>
2023-02-02 09:46:42 -0800

    report card plus

---
+++ codesprint/2023/reportcardplus.cpp
@@ -0,0 +1,181 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, j, k) for (int i=j ; i<k ; ++i)
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define FORI(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k) for (int i=j ; i>=k ; --i)
+#define RFORI(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j)
+#define RREP(i, j) RFOR(i, j, 0)
+#define OREP(i, j) FOR(i, 1, j+1)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define PB push_back
+#define F first
+#define S second
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+template<typename T> inline void printV(vector<T> &vec) {
+    cerr << "########\n";
+    for (int i = 0; i < vec.size(); ++i) {
+        cerr << vec[i] << ' ';
+    }
+    cerr << '\n';
+    cerr << "########\n";
+}
+template<typename T> inline void printVV(vector<vector<T>> &vec) {
+    cerr << "########\n";
+    for (int i = 0; i < vec.size(); ++i) {
+        for (int j = 0; j < vec[i].size(); ++j) {
+            cerr << vec[i][j] << ' ';
+        }
+        cerr << '\n';
+    }
+    cerr << "########\n";
+}
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+template<typename T> inline void dbg(T obj) { cerr << obj << '\n'; }
+template<typename T, typename... Args> inline void dbg(T t, Args... args) { cerr << t << " "; dbg(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+const ld pi = 3.1415926535897932384626433832795;
+const int mod = 1000000007;
+
+typedef complex<double> C;
+typedef vector<double> vd;
+typedef vector<ll> vl;
+void fft(vector<C>& a) {
+	int n = sz(a), L = 31 - __builtin_clz(n);
+	static vector<complex<long double>> R(2, 1);
+	static vector<C> rt(2, 1);  // (^ 10% faster if double)
+	for (static int k = 2; k < n; k *= 2) {
+		R.resize(n); rt.resize(n);
+		auto x = polar(1.0L, acos(-1.0L) / k);
+		rep(i,k,2*k) rt[i] = R[i] = i&1 ? R[i/2] * x : R[i/2];
+	}
+	vi rev(n);
+	rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+	rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+	for (int k = 1; k < n; k *= 2)
+		for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
+			// C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
+			auto x = (double *)&rt[j+k], y = (double *)&a[i+j+k];        /// exclude-line
+			C z(x[0]*y[0] - x[1]*y[1], x[0]*y[1] + x[1]*y[0]);           /// exclude-line
+			a[i + j + k] = a[i + j] - z;
+			a[i + j] += z;
+		}
+}
+vd conv(const vd& a, const vd& b) {
+	if (a.empty() || b.empty()) return {};
+	vd res(sz(a) + sz(b) - 1);
+	int L = 32 - __builtin_clz(sz(res)), n = 1 << L;
+	vector<C> in(n), out(n);
+	copy(all(a), begin(in));
+	rep(i,0,sz(b)) in[i].imag(b[i]);
+	fft(in);
+	for (C& x : in) x *= x;
+	rep(i,0,n) out[i] = in[-i & (n - 1)] - conj(in[i]);
+	fft(out);
+	rep(i,0,sz(res)) res[i] = imag(out[i]) / (4 * n);
+	return res;
+}
+template<int M> vl convMod(const vl &a, const vl &b) {
+	if (a.empty() || b.empty()) return {};
+	vl res(sz(a) + sz(b) - 1);
+	int B=32-__builtin_clz(sz(res)), n=1<<B, cut=int(sqrt(M));
+	vector<C> L(n), R(n), outs(n), outl(n);
+	rep(i,0,sz(a)) L[i] = C((int)a[i] / cut, (int)a[i] % cut);
+	rep(i,0,sz(b)) R[i] = C((int)b[i] / cut, (int)b[i] % cut);
+	fft(L), fft(R);
+	rep(i,0,n) {
+		int j = -i & (n - 1);
+		outl[j] = (L[i] + conj(L[j])) * R[i] / (2.0 * n);
+		outs[j] = (L[i] - conj(L[j])) * R[i] / (2.0 * n) / 1i;
+	}
+	fft(outl), fft(outs);
+	rep(i,0,sz(res)) {
+		ll av = ll(real(outl[i])+.5), cv = ll(imag(outs[i])+.5);
+		ll bv = ll(imag(outl[i])+.5) + ll(real(outs[i])+.5);
+		res[i] = ((av % M * cut + bv) % M * cut + cv) % M;
+	}
+	return res;
+}
+
+void run() {
+    int n; cin >> n;
+    string s; cin >> s;
+    VVLL polys;
+    int ct = 0;
+    REP(i, n) {
+        if (s[i] == 'F') {
+            VLL v = {0, 1, 0};
+            polys.PB(v);
+        }
+        else if (s[i] == '?') {
+            VLL v = {4, 6, 3};
+            polys.PB(v);
+        } else if (s[i] == 'A') {
+            VLL v = {1, 2, 0};
+            polys.PB(v);
+        } else {
+            VLL v = {1, 1, 1};
+            polys.PB(v);
+        }
+    }
+
+    while (polys.size() > 1) {
+        VVLL np;
+        int k = polys.size();
+        for (int i = 0; i < k; i += 2) {
+            if (i == k-1) {
+                np.PB(polys[i]);
+                continue;
+            }
+            VLL r = convMod<mod>(polys[i], polys[i+1]);
+            np.PB(r);
+        }
+        polys = np;
+    }
+    VLL res = polys[0];
+    // # of ways to have 0 = res[n];
+    ll soln = 0;
+    for (int i = n+1; i < res.size(); ++i) {
+        soln += res[i];
+        soln %= mod;
+    }
+    print(soln);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(15);
+    // ll t; cin >> t;
+    ll t=1;
+    REP(tests,t) run();
+}
