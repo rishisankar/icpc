6ce63f516eb7327752f3d2b6eb8eec02464d3162
Rishi Sankar <rishi.sankar@gmail.com>
2023-03-18 19:17:51 -0700

    cses advanced flow/hungarian

---
+++ cses/advanced/parcel_delivery.cpp
@@ -0,0 +1,190 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) 
+{ os << '{'; string sep; for (const auto &x: v) os << sep << x, sep = ", "; return os << '}';}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) 
+{ return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#ifdef TTL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+VPII dirs{mp(-1,0),mp(1,0),mp(0,-1),mp(0,1)};
+const ld pi = 3.1415926535897932384626433832795;
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+
+const ll INF = numeric_limits<ll>::max() / 4;
+typedef vector<ll> VL;
+
+struct MCMF {
+	int N;
+	vector<vi> ed, red;
+	vector<VL> cap, flow, cost;
+	vi seen;
+	VL dist, pi;
+	vector<pii> par;
+
+	MCMF(int N) :
+		N(N), ed(N), red(N), cap(N, VL(N)), flow(cap), cost(cap),
+		seen(N), dist(N), pi(N), par(N) {}
+
+	void addEdge(int from, int to, ll cap, ll cost) {
+		this->cap[from][to] = cap;
+		this->cost[from][to] = cost;
+		ed[from].push_back(to);
+		red[to].push_back(from);
+	}
+
+	void path(int s) {
+		fill(all(seen), 0);
+		fill(all(dist), INF);
+		dist[s] = 0; ll di;
+
+		__gnu_pbds::priority_queue<pair<ll, int>> q;
+		vector<decltype(q)::point_iterator> its(N);
+		q.push({0, s});
+
+		auto relax = [&](int i, ll cap, ll cost, int dir) {
+			ll val = di - pi[i] + cost;
+			if (cap && val < dist[i]) {
+				dist[i] = val;
+				par[i] = {s, dir};
+				if (its[i] == q.end()) its[i] = q.push({-dist[i], i});
+				else q.modify(its[i], {-dist[i], i});
+			}
+		};
+
+		while (!q.empty()) {
+			s = q.top().second; q.pop();
+			seen[s] = 1; di = dist[s] + pi[s];
+			for (int i : ed[s]) if (!seen[i])
+				relax(i, cap[s][i] - flow[s][i], cost[s][i], 1);
+			for (int i : red[s]) if (!seen[i])
+				relax(i, flow[i][s], -cost[i][s], 0);
+		}
+		rep(i,0,N) pi[i] = min(pi[i] + dist[i], INF);
+	}
+
+	pair<ll, ll> maxflow(int s, int t) {
+		ll totflow = 0, totcost = 0;
+		while (path(s), seen[t]) {
+			ll fl = INF;
+			for (int p,r,x = t; tie(p,r) = par[x], x != s; x = p)
+				fl = min(fl, r ? cap[p][x] - flow[p][x] : flow[x][p]);
+			totflow += fl;
+			for (int p,r,x = t; tie(p,r) = par[x], x != s; x = p)
+				if (r) flow[p][x] += fl;
+				else flow[x][p] -= fl;
+		}
+		rep(i,0,N) rep(j,0,N) totcost += cost[i][j] * flow[i][j];
+		return {totflow, totcost};
+	}
+
+	// If some costs can be negative, call this before maxflow:
+	void setpi(int s) { // (otherwise, leave this out)
+		fill(all(pi), INF); pi[s] = 0;
+		int it = N, ch = 1; ll v;
+		while (ch-- && it--)
+			rep(i,0,N) if (pi[i] != INF)
+				for (int to : ed[i]) if (cap[i][to])
+					if ((v = pi[i] + cost[i][to]) < pi[to])
+						pi[to] = v, ch = 1;
+		assert(it >= 0); // negative cost cycle
+	}
+};
+
+
+// handles duplicate edges too! default does not...
+void run() {
+    int n,m,k; cin >> n >> m >> k;
+    vector<tuple<int,int,int,int>> edges;
+    map<pii,int> edgeCounts;
+    rep(i,0,m) {
+        int a,b,r,c; cin >> a >> b >> r >> c;
+        --a; --b;
+        edges.pb({a,b,r,c});
+        edgeCounts[{a,b}]++;
+    }
+    vector<int> dups(n,1);
+    for (auto p : edgeCounts) {
+        dups[p.F.S] = max(dups[p.F.S], p.S);
+    }
+    vector<int> pfs(n);
+    pfs[0] = dups[0];
+    rep(i,1,n) pfs[i] = pfs[i-1] + dups[i];
+    // ex: dups = 1 3 2, pfs = 1 4 6 ==> node [0,1) for 0, [1,4) for 1, [4,6) for 2
+    MCMF mf(pfs[n-1]+1);
+    rep(i,0,n) {
+        rep(j,1,dups[i]) {
+            int fst = (i == 0 ? 0 : pfs[i-1]);
+            mf.addEdge(fst+j-1, fst+j, k+5, 0);
+        }
+    }
+    map<pii,int> ec;
+    for (tuple<int,int,int,int> t : edges) {
+        int a,b,r,c;
+        tie(a,b,r,c) = t;
+        int z = ec[{a,b}];
+        ec[{a,b}]++;
+        int fst = (b == 0 ? 0 : pfs[b-1]);
+        mf.addEdge(pfs[a]-1, fst+z, r, c);
+    }
+    mf.addEdge(pfs[n-1]-1,pfs[n-1],k,0);
+    pll ans = mf.maxflow(0, pfs[n-1]);
+    if (ans.F < k) print(-1);
+    else print(ans.S);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(15);
+    // ll t; cin >> t;
+    ll t=1;
+    rep(tests,0,t) run();
+}
---
+++ cses/advanced/task_assignment.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) 
+{ os << '{'; string sep; for (const auto &x: v) os << sep << x, sep = ", "; return os << '}';}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) 
+{ return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#ifdef TTL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+VPII dirs{mp(-1,0),mp(1,0),mp(0,-1),mp(0,1)};
+const ld pi = 3.1415926535897932384626433832795;
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+
+pair<int, vi> hungarian(const vector<vi> &a) {
+	if (a.empty()) return {0, {}};
+	int n = sz(a) + 1, m = sz(a[0]) + 1;
+	vi u(n), v(m), p(m), ans(n - 1);
+	rep(i,1,n) {
+		p[0] = i;
+		int j0 = 0; // add "dummy" worker 0
+		vi dist(m, INT_MAX), pre(m, -1);
+		vector<bool> done(m + 1);
+		do { // dijkstra
+			done[j0] = true;
+			int i0 = p[j0], j1, delta = INT_MAX;
+			rep(j,1,m) if (!done[j]) {
+				auto cur = a[i0 - 1][j - 1] - u[i0] - v[j];
+				if (cur < dist[j]) dist[j] = cur, pre[j] = j0;
+				if (dist[j] < delta) delta = dist[j], j1 = j;
+			}
+			rep(j,0,m) {
+				if (done[j]) u[p[j]] += delta, v[j] -= delta;
+				else dist[j] -= delta;
+			}
+			j0 = j1;
+		} while (p[j0]);
+		while (j0) { // update alternating path
+			int j1 = pre[j0];
+			p[j0] = p[j1], j0 = j1;
+		}
+	}
+	rep(j,1,m) if (p[j]) ans[p[j] - 1] = j - 1;
+	return {-v[0], ans}; // min cost
+}
+
+
+void run() {
+    int n; cin >> n;
+    VVI c(n,VI(n));
+    rep(i,0,n) {
+        rep(j,0,n) {
+            cin >> c[i][j];
+        }
+    }
+    auto p = hungarian(c);
+    print(p.F);
+    rep(i,0,n) {
+        print(i+1,p.S[i]+1);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(15);
+    // ll t; cin >> t;
+    ll t=1;
+    rep(tests,0,t) run();
+}
