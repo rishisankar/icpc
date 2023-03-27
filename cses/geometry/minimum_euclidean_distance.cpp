bba9bb0e3146d00d0f1b8dafd388981543cbdef5
Rishi Sankar <rishi.sankar@gmail.com>
2023-03-27 16:16:43 -0700

    +2 geo cses

---
+++ cses/geometry/minimum_euclidean_distance.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) 
+{ os << '{'; string sep; for (const auto &x: v) os << sep << x, sep = ", "; return os << '}';}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) 
+{ return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#ifdef TTL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+VPII dirs{mp(-1,0),mp(1,0),mp(0,-1),mp(0,1)};
+const ld pi = 3.1415926535897932384626433832795;
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+
+template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
+template<class T>
+struct Point {
+	typedef Point P;
+	T x, y;
+	explicit Point(T x=0, T y=0) : x(x), y(y) {}
+	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+	P operator+(P p) const { return P(x+p.x, y+p.y); }
+	P operator-(P p) const { return P(x-p.x, y-p.y); }
+	P operator*(T d) const { return P(x*d, y*d); }
+	P operator/(T d) const { return P(x/d, y/d); }
+	T dot(P p) const { return x*p.x + y*p.y; }
+	T cross(P p) const { return x*p.y - y*p.x; }
+	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+	T dist2() const { return x*x + y*y; }
+	double dist() const { return sqrt((double)dist2()); }
+	// angle to x-axis in interval [-pi, pi]
+	double angle() const { return atan2(y, x); }
+	P unit() const { return *this/dist(); } // makes dist()=1
+	P perp() const { return P(-y, x); } // rotates +90 degrees
+	P normal() const { return perp().unit(); }
+	// returns point rotated 'a' radians ccw around the origin
+	P rotate(double a) const {
+		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+	friend ostream& operator<<(ostream& os, P p) {
+		return os << "(" << p.x << "," << p.y << ")"; }
+};
+
+typedef Point<ll> P;
+pair<P, P> closest(vector<P> v) {
+	assert(sz(v) > 1);
+	set<P> S;
+	sort(all(v), [](P a, P b) { return a.y < b.y; });
+	pair<ll, pair<P, P>> ret{LLONG_MAX, {P(), P()}};
+	int j = 0;
+	for (P p : v) {
+		P d{1 + (ll)sqrt(ret.first), 0};
+		while (v[j].y <= p.y - d.x) S.erase(v[j++]);
+		auto lo = S.lower_bound(p - d), hi = S.upper_bound(p + d);
+		for (; lo != hi; ++lo)
+			ret = min(ret, {(*lo - p).dist2(), {*lo, p}});
+		S.insert(p);
+	}
+	return ret.second;
+}
+
+void run() {
+    int n; cin >> n;
+    vector<P> pts;
+    rep(i,0,n) {
+        ll x,y; cin >> x >> y; pts.emplace_back(x,y);
+    }
+    auto p = closest(pts);
+    print((p.F.x - p.S.x)*(p.F.x - p.S.x) + (p.F.y - p.S.y)*(p.F.y - p.S.y));
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(15);
+    // ll t; cin >> t;
+    ll t=1;
+    rep(tests,0,t) run();
+}
---
+++ cses/geometry/point_in_polygon.cpp
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) 
+{ os << '{'; string sep; for (const auto &x: v) os << sep << x, sep = ", "; return os << '}';}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) 
+{ return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#ifdef TTL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+VPII dirs{mp(-1,0),mp(1,0),mp(0,-1),mp(0,1)};
+const ld pi = 3.1415926535897932384626433832795;
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+
+template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
+template<class T>
+struct Point {
+	typedef Point P;
+	T x, y;
+	explicit Point(T x=0, T y=0) : x(x), y(y) {}
+	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+	P operator+(P p) const { return P(x+p.x, y+p.y); }
+	P operator-(P p) const { return P(x-p.x, y-p.y); }
+	P operator*(T d) const { return P(x*d, y*d); }
+	P operator/(T d) const { return P(x/d, y/d); }
+	T dot(P p) const { return x*p.x + y*p.y; }
+	T cross(P p) const { return x*p.y - y*p.x; }
+	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+	T dist2() const { return x*x + y*y; }
+	double dist() const { return sqrt((double)dist2()); }
+	// angle to x-axis in interval [-pi, pi]
+	double angle() const { return atan2(y, x); }
+	P unit() const { return *this/dist(); } // makes dist()=1
+	P perp() const { return P(-y, x); } // rotates +90 degrees
+	P normal() const { return perp().unit(); }
+	// returns point rotated 'a' radians ccw around the origin
+	P rotate(double a) const {
+		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+	friend ostream& operator<<(ostream& os, P p) {
+		return os << "(" << p.x << "," << p.y << ")"; }
+};
+
+typedef Point<double> P;
+double segDist(P& s, P& e, P& p) {
+	if (s==e) return (p-s).dist();
+	auto d = (e-s).dist2(), t = min(d,max(.0,(p-s).dot(e-s)));
+	return ((p-s)*d-(e-s)*t).dist()/d;
+}
+
+template<class P>
+int sideOf(P s, P e, P p) { return sgn(s.cross(e, p)); }
+
+template<class P>
+int sideOf(const P& s, const P& e, const P& p, double eps) {
+	auto a = (e-s).cross(p-s);
+	double l = (e-s).dist()*eps;
+	return (a > l) - (a < -l);
+}
+
+template<class P> bool onSegment(P s, P e, P p) {
+	return p.cross(s, e) == 0 && (s - p).dot(e - p) <= 0;
+}
+
+template<class P>
+bool inPolygon(vector<P> &p, P a, bool strict = true) {
+	int cnt = 0, n = sz(p);
+	rep(i,0,n) {
+		P q = p[(i + 1) % n];
+		if (onSegment(p[i], q, a)) return !strict;
+		//or: if (segDist(p[i], q, a) <= eps) return !strict;
+		cnt ^= ((a.y<p[i].y) - (a.y<q.y)) * a.cross(p[i], q) > 0;
+	}
+	return cnt;
+}
+
+template<class T>
+T polygonArea2(vector<Point<T>>& v) {
+	T a = v.back().cross(v[0]);
+	rep(i,0,sz(v)-1) a += v[i].cross(v[i+1]);
+	return a;
+}
+
+void run() {
+    int n,m; cin >> n >> m;
+    vector<P> pts;
+    rep(i,0,n) {
+        ll a,b; cin >> a >> b;
+        pts.emplace_back(a,b);
+    }
+    if (polygonArea2(pts) < 0) reverse(all(pts));
+    rep(i,0,m) {
+        ll a,b; cin >> a >> b; P p(a,b);
+        if (inPolygon(pts,p,true)) {
+            print("INSIDE");
+        } else if (inPolygon(pts,p,false)) {
+            print("BOUNDARY");
+        } else {
+            print("OUTSIDE");
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(15);
+    // ll t; cin >> t;
+    ll t=1;
+    rep(tests,0,t) run();
+}
