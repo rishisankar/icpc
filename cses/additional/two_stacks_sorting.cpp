b363911e49bbc18629a9e17c5859d32243106c3d
Rishi Sankar <rishi.sankar@gmail.com>
2023-03-29 11:01:14 -0700

    two stacks sorting initial ideas

---
+++ cses/additional/two_stacks_sorting.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) 
+{ os << '{'; string sep; for (const auto &x: v) os << sep << x, sep = ", "; return os << '}';}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) 
+{ return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#ifdef TTL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+VPII dirs{mp(-1,0),mp(1,0),mp(0,-1),mp(0,1)};
+const ld pi = 3.1415926535897932384626433832795;
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+
+/*
+
+Observations:
+- can never place a number on top of something smaller
+- if 1...k are all placed, they are all cleared from the stack
+
+Need to maintain:
+- priority queue of numbers in both stacks (each # is popped at most once!)
+
+For each number:
+    - identify k s.t. 1...k are solved (segment tree etc)
+    - pop as much of 1..k as possible from both stacks' pq
+    - place on stack (greedily pick smaller of the two) *** wrong
+    - if can't put on either stack, fail
+Succeed if all numbers placed on stacks
+
+place on stack (greedily pick smaller of the two):
+
+5 10 --> 2 10 (place 2 on lower) --> 2 4 (place 4 on only choice) --> 1 (2 gets cleared) --> 5 4
+5 10 --> 5 2 (place 2 on higher) --> 4 2 (place 4 on only chocie) --> 1 (2 gets cleared) --> 4 10 (better...)
+
+
+instead: when placing a #, need to know when it will be cleared + is it possible to build two sequences until then
+(maybe largest LIS?)
+
+
+for each # can u find the point where all previous #s are included
+
+*/
+
+void run() {
+    // int n; cin >> n; VLL v(n); INP(v,n);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(15);
+    // ll t; cin >> t;
+    ll t=1;
+    rep(tests,0,t) run();
+}
