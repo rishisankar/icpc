a5bbc970c0972f64eed535750fa11218f4220cdc
Rishi Sankar <rishi.sankar@gmail.com>
2023-03-28 23:02:04 -0700

    subarray squares (D&C dp)

---
+++ cses/advanced/subarray_squares.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) 
+{ os << '{'; string sep; for (const auto &x: v) os << sep << x, sep = ", "; return os << '}';}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) 
+{ return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#ifdef TTL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+VPII dirs{mp(-1,0),mp(1,0),mp(0,-1),mp(0,1)};
+const ld pi = 3.1415926535897932384626433832795;
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+
+/*
+
+dp[i][j]: separate first i elements into j subarrays (assume i >= j)
+if j == 1: only one possibility
+if j > 1:
+    dp[i][j] = min{k = 1...(i+1-j)}: dp[i-k][j-1] + sum of last subarray
+
+D&C dp check: as i increases (# elts), does split point increase monotonically?
+
+Quadrangle inequality: C(a,c) + C(b,d) <= C(a,d) + C(b,c) for a<=b<=c<=d
+C(k,i) = (v[i] + ... + v[i-k+1])^2
+
+
+For j = ...
+- f(i,k) := separate elements up to v[i] into j subarrays where jth starts at index k
+
+
+*/
+
+VVLL dp;
+VLL pf;
+
+struct DP { // Modify at will:
+    int J;
+	int lo(int ind) { return J-1; }
+	int hi(int ind) { return ind+1; } // not inclusive!!
+	ll f(int ind, int k) { 
+        ll ans = dp[k-1][J-1] + (pf[ind]-pf[k-1]) * (pf[ind]-pf[k-1]);
+        // dbg("f",ind,k,ans);
+        return ans;
+    }
+	void store(int ind, int k, ll v) {
+        // dbg(ind,J,k,v);
+        dp[ind][J] = v;
+    }
+
+	void rec(int L, int R, int LO, int HI) {
+		if (L >= R) return;
+		int mid = (L + R) >> 1;
+		pair<ll, int> best(LLONG_MAX, LO);
+		rep(k, max(LO,lo(mid)), min(HI,hi(mid)))
+			best = min(best, make_pair(f(mid, k), k));
+		store(mid, best.second, best.first);
+		rec(L, mid, LO, best.second+1);
+		rec(mid+1, R, best.second, HI);
+	}
+	void solve(int L, int R) { rec(L, R, INT_MIN, INT_MAX); } // not inclusive on R!!
+};
+
+void run() {
+    int n,k; cin >> n >> k;
+    dp.assign(n, VLL(k+1, -1));
+    VLL v(n); INP(v,n);
+    pf.resize(n);
+    pf[0] = v[0];
+    rep(i,1,n) pf[i] = pf[i-1] + v[i];
+    DP dac;
+    rep(i,0,n) dp[i][1] = pf[i]*pf[i];
+    rep(j,2,k+1) {
+        dac.J = j;
+        // dbg(j-1,n);
+        dac.solve(j-1,n);
+    }
+    print(dp[n-1][k]);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(15);
+    // ll t; cin >> t;
+    ll t=1;
+    rep(tests,0,t) run();
+}
