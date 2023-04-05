6f056e85f75488347468621509c074ea6393e233
Rishi Sankar <rishi.sankar@gmail.com>
2023-04-04 20:15:05 -0700

    school excursion

---
+++ cses/additional/school_excursion.cpp
@@ -0,0 +1,131 @@
+// too slow but complexity should be better :(
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) 
+{ os << '{'; string sep; for (const auto &x: v) os << sep << x, sep = ", "; return os << '}';}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) 
+{ return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#ifdef TTL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+VPII dirs{mp(-1,0),mp(1,0),mp(0,-1),mp(0,1)};
+const ld pi = 3.1415926535897932384626433832795;
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+
+/*
+vector of all component sizes
+
+bitset optimization
+https://usaco.guide/plat/bitsets?lang=cpp
+
+knapsack optimization:
+for a given i, if there are >= 3 items then take out as many as possible (multiple of 2) but leave 1;
+    send the rest to 2*i
+    this will ensure O(sqrt(n)) OR operations!
+
+==> overall complexity O(n^1.5 / 64)
+
+(also have fft solution in O(n log^3(n)), but too slow given bounds)
+*/
+
+struct UF {
+	vi e;
+	UF(int n) : e(n, -1) {}
+	bool sameSet(int a, int b) { return find(a) == find(b); }
+	int size(int x) { return -e[find(x)]; }
+	int find(int x) { return e[x] < 0 ? x : e[x] = find(e[x]); }
+	bool join(int a, int b) {
+		a = find(a), b = find(b);
+		if (a == b) return false;
+		if (e[a] > e[b]) swap(a, b);
+		e[a] += e[b]; e[b] = a;
+		return true;
+	}
+};
+
+void run() {
+    int n,m; cin >> n >> m;
+    UF u(n);
+    rep(i,0,m) {
+        int a,b; cin >> a >> b;
+        u.join(a-1,b-1);
+    }
+    vi comps;
+    vi cts(n+1);
+    rep(i,0,n) {
+        if (i == u.find(i)) {
+            cts[u.size(i)]++;
+        }
+    }
+    rep(i,1,n+1) {
+        if (cts[i] >= 3) {
+            // optimization: push as many as possible into 2i (save for at least 1)
+            int k = (cts[i]-1)/2;
+            cts[i] -= 2*k;
+            cts[2*i] += k;
+        }
+    }
+    bitset<100001> bs;
+    bs[0] = 1;
+    rep(i,1,n+1) {
+        rep(j,0,cts[i]) bs |= (bs<<i);
+    }
+    rep(i,1,n+1) cout << bs[i];
+    cout << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(15);
+    // ll t; cin >> t;
+    ll t=1;
+    rep(tests,0,t) run();
+}
---
+++ cses/additional/school_excursion_fft.cpp
@@ -0,0 +1,166 @@
+// too slow but complexity should be better :(
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) 
+{ os << '{'; string sep; for (const auto &x: v) os << sep << x, sep = ", "; return os << '}';}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) 
+{ return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#ifdef TTL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+VPII dirs{mp(-1,0),mp(1,0),mp(0,-1),mp(0,1)};
+const ld pi = 3.1415926535897932384626433832795;
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+
+/*
+vector of all component sizes
+
+component of size c represented by polynomial 1+x^c
+    - multiplication of n polynomials with degree sum O(n) can be done in n log^2 n
+
+or bitset optimization
+*/
+
+typedef complex<double> C;
+typedef vector<double> vd;
+void fft(vector<C>& a) {
+	int n = sz(a), L = 31 - __builtin_clz(n);
+	static vector<complex<long double>> R(2, 1);
+	static vector<C> rt(2, 1);  // (^ 10% faster if double)
+	for (static int k = 2; k < n; k *= 2) {
+		R.resize(n); rt.resize(n);
+		auto x = polar(1.0L, acos(-1.0L) / k);
+		rep(i,k,2*k) rt[i] = R[i] = i&1 ? R[i/2] * x : R[i/2];
+	}
+	vi rev(n);
+	rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+	rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+	for (int k = 1; k < n; k *= 2)
+		for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
+			// C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
+			auto x = (double *)&rt[j+k], y = (double *)&a[i+j+k];        /// exclude-line
+			C z(x[0]*y[0] - x[1]*y[1], x[0]*y[1] + x[1]*y[0]);           /// exclude-line
+			a[i + j + k] = a[i + j] - z;
+			a[i + j] += z;
+		}
+}
+vd conv(const vd& a, const vd& b) {
+	if (a.empty() || b.empty()) return {};
+	vd res(sz(a) + sz(b) - 1);
+	int L = 32 - __builtin_clz(sz(res)), n = 1 << L;
+	vector<C> in(n), out(n);
+	copy(all(a), begin(in));
+	rep(i,0,sz(b)) in[i].imag(b[i]);
+	fft(in);
+	for (C& x : in) x *= x;
+	rep(i,0,n) out[i] = in[-i & (n - 1)] - conj(in[i]);
+	fft(out);
+	rep(i,0,sz(res)) res[i] = imag(out[i]) / (4 * n);
+	return res;
+}
+
+struct UF {
+	vi e;
+	UF(int n) : e(n, -1) {}
+	bool sameSet(int a, int b) { return find(a) == find(b); }
+	int size(int x) { return -e[find(x)]; }
+	int find(int x) { return e[x] < 0 ? x : e[x] = find(e[x]); }
+	bool join(int a, int b) {
+		a = find(a), b = find(b);
+		if (a == b) return false;
+		if (e[a] > e[b]) swap(a, b);
+		e[a] += e[b]; e[b] = a;
+		return true;
+	}
+};
+
+void run() {
+    int n,m; cin >> n >> m;
+    UF u(n);
+    rep(i,0,m) {
+        int a,b; cin >> a >> b;
+        u.join(a-1,b-1);
+    }
+    vi comps;
+    rep(i,0,n) {
+        if (i == u.find(i)) {
+            comps.pb(u.size(i));
+        }
+    }
+    auto sorter = [&](vd& v1, vd& v2) {
+        return sz(v1) < sz(v2);
+    };
+    priority_queue<vd, vector<vd>, decltype(sorter)> pq(sorter);
+    for (int i : comps) {
+        vd poly(i+1);
+        poly[0] = poly[i] = 1;
+        pq.push(poly);
+    }
+    while (pq.size() >= 2) {
+        vd t1 = pq.top(); pq.pop();
+        vd t2 = pq.top(); pq.pop();
+        vd comb = conv(t1,t2);
+        pq.push(comb);
+    }
+    vd res = pq.top();
+    rep(i,1,n+1) {
+        cout << (res[i]>0.5);
+    }
+    cout << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(15);
+    // ll t; cin >> t;
+    ll t=1;
+    rep(tests,0,t) run();
+}
