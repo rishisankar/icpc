3ec2e56a92b10519ac2be5e180a6fde8cdac0daa
Rishi Sankar <rishi.sankar@gmail.com>
2023-04-06 22:31:12 -0700

    practices

---
+++ practices2023/calgary2017/gorillas.cpp
@@ -0,0 +1,155 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) 
+{ os << '{'; string sep; for (const auto &x: v) os << sep << x, sep = ", "; return os << '}';}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) 
+{ return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#ifdef TTL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+VPII dirs{mp(-1,0),mp(1,0),mp(0,-1),mp(0,1)};
+const ld pi = 3.1415926535897932384626433832795;
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+
+typedef vector<ld> vd;
+const ld eps = 1e-15;
+
+int solveLinear(vector<vd>& A, vd& b, vd& x) {
+	int n = sz(A), m = sz(x), rank = 0, br, bc;
+	if (n) assert(sz(A[0]) == m);
+	vi col(m); iota(all(col), 0);
+
+	rep(i,0,n) {
+		ld v, bv = 0;
+		rep(r,i,n) rep(c,i,m)
+			if ((v = fabs(A[r][c])) > bv)
+				br = r, bc = c, bv = v;
+		if (bv <= eps) {
+			rep(j,i,n) if (fabs(b[j]) > eps) return -1;
+			break;
+		}
+		swap(A[i], A[br]);
+		swap(b[i], b[br]);
+		swap(col[i], col[bc]);
+		rep(j,0,n) swap(A[j][i], A[j][bc]);
+		bv = 1/A[i][i];
+		rep(j,i+1,n) {
+			ld fac = A[j][i] * bv;
+			b[j] -= fac * b[i];
+			rep(k,i+1,m) A[j][k] -= fac*A[i][k];
+		}
+		rank++;
+	}
+
+	x.assign(m, 0);
+	for (int i = rank; i--;) {
+		b[i] /= A[i][i];
+		x[col[i]] = b[i];
+		rep(j,0,i) b[j] -= A[j][i] * b[i];
+	}
+	return rank; // (multiple solutions if rank < m)
+}
+
+void run() {
+    ld xs,ys,xt,yt; cin >> xs >> ys >> xt >> yt;
+    if (xs > xt) {
+        swap(xs,xt);
+        swap(ys,yt);
+    }
+    int t; cin >> t;
+    vector<pll> v(t);
+    rep(i,0,t) {
+        cin >> v[i].F >> v[i].S;
+    }
+    ld lo = 0, hi = 1000000000000000.L;
+    ld bst = hi;
+    rep(i,0,10000) {
+        ld mid = lo+(hi-lo)/2;
+        vector<vd> A{
+            {2*xs,1,0},
+            {xs*xs,xs,1},
+            {xt*xt,xt,1}
+        };
+        vd b{mid,ys,yt};
+        vd x(3);
+        int rank = solveLinear(A,b,x);
+        assert(rank != -1);
+        bool ok = true;
+        if (x[0] > 0) ok = false;
+        if (ok) {
+            for (pll p : v) {
+                ld y = p.F*p.F*x[0] + p.F*x[1] + x[2];
+                if (p.S > y) {
+                    ok = false;
+                    break;
+                }
+            }
+        }
+        if (ok) {
+            ld vx = (-x[1]/2/x[0]);
+            if (xs <= vx && vx <= xt) 
+            bst = min(bst,x[0]*vx*vx + x[1]*vx + x[2]);
+            else bst = min(bst, max(yt,ys));
+
+            hi = mid;
+        } else {
+            lo = mid;
+        }
+    }
+    cout << setprecision(20) << bst << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(15);
+    ll t; cin >> t;
+    // ll t=1;
+    rep(tests,0,t) run();
+}
