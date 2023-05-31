b0af09cb4f7b8d19bc3d5bcf2f9b059c0cdc1a5d
Rishi Sankar <rishi.sankar@gmail.com>
2023-05-31 07:56:43 -0700

    lca template w path stuff

---
+++ algorithms/graphs/trees/lca.cpp
@@ -0,0 +1,48 @@
+template<class T>
+struct RMQ {
+	vector<vector<T>> jmp;
+	RMQ(const vector<T>& V) : jmp(1, V) {
+		for (int pw = 1, k = 1; pw * 2 <= sz(V); pw *= 2, ++k) {
+			jmp.emplace_back(sz(V) - pw * 2 + 1);
+			rep(j,0,sz(jmp[k]))
+				jmp[k][j] = min(jmp[k - 1][j], jmp[k - 1][j + pw]);
+		}
+	}
+	T query(int a, int b) {
+		assert(a < b); // or return inf if a == b
+		int dep = 31 - __builtin_clz(b - a);
+		return min(jmp[dep][a], jmp[dep][b - (1 << dep)]);
+	}
+};
+
+struct LCA {
+	int T = 0;
+	vi time, path, ret;
+	RMQ<int> rmq;
+
+	LCA(vector<vi>& C) : time(sz(C)), rmq((dfs(C,0,-1), ret)) {}
+	void dfs(vector<vi>& C, int v, int par) {
+		time[v] = T++;
+		for (int y : C[v]) if (y != par) {
+			path.push_back(v), ret.push_back(time[v]);
+			dfs(C, y, v);
+		}
+	}
+
+	int lca(int a, int b) {
+		if (a == b) return a;
+		tie(a, b) = minmax(time[a], time[b]);
+		return path[rmq.query(a, b)];
+	}
+
+  bool on_path(int u, int v, int x) {
+    return (lca(x,u) == lca(u,v) && lca(x,v) == x) || (lca(x,v) == lca(u,v) && lca(x,u) == x);
+  }
+
+  /*
+  check if paths from u->v and a->b on tree are disjoint
+  */
+  bool paths_disjoint(int u, int v, int a, int b) {
+    return !(on_path(a,b,lca(u,v)) || on_path(u,v,lca(a,b)));
+  }
+};
