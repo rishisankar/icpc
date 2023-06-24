2e9f616594a195d8859f20015b064469a858566a
Rishi Sankar <rishi.sankar@gmail.com>
2023-06-24 11:10:21 -0700

    codeton round 5 saj

---
+++ codeforces/codeton-5/B.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+struct custom_hash { // https://codeforces.com/blog/entry/62393
+    static uint64_t splitmix64(uint64_t x) { x += 0x9e3779b97f4a7c15; x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9; x = (x ^ (x >> 27)) * 0x94d049bb133111eb; return x ^ (x >> 31); }
+    size_t operator()(uint64_t x) const { static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count(); return splitmix64(x + FIXED_RANDOM); }
+};
+template<typename T, typename U> struct safe_map : public std::unordered_map<T,U,custom_hash> {};
+template<typename T> struct safe_set : public std::unordered_set<T,custom_hash> {};
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) 
+{ os << '{'; string sep; for (const auto &x: v) os << sep << x, sep = ", "; return os << '}';}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) 
+{ return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#ifdef TTL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+VPII dirs{mp(-1,0),mp(1,0),mp(0,-1),mp(0,1)};
+const ld pi = 3.1415926535897932384626433832795;
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+
+void test(ll& cur, ll target, VLL& v) {
+    if (sz(v)==0)return;
+    ll x = v.back();
+    ll y = cur|x;
+    rep(i,0,30) {
+        if ((y>>i)&1) {
+            if (((target>>i)&1) == 0) {
+                dbg(i,y,target);
+                v.clear();
+                return;
+            }
+        }
+    }
+    v.pop_back();
+    cur|=x;
+}
+
+void run() {
+    int n; ll x; cin >> n >> x;
+    dbg(n,x);
+    ll cur = 0;
+    VLL a(n),b(n),c(n); INP(a,n);INP(b,n); INP(c,n);
+    reverse(all(a));
+    reverse(all(b));
+    reverse(all(c));
+    while (sz(a)>0||sz(b)>0||sz(c)>0) {
+        dbg(sz(a),sz(b),sz(c),cur);
+        test(cur,x,a);
+        test(cur,x,b);
+        test(cur,x,c);
+    }
+    if (cur==x) print("Yes");
+    else print("No");
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(15);
+    ll t; cin >> t;
+    // ll t=1;
+    rep(tests,0,t) run();
+}
---
+++ codeforces/codeton-5/E.cpp
@@ -0,0 +1,152 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define INP(v, n) for (int i=0; i<n; ++i) { cin >> v[i]; }
+#define ceil(x) (ll)ceil(x)
+#define floor(x) (ll)floor(x)
+
+template<typename T> inline T maxe(vector<T> &vec) { return *max_element(all(vec)); }
+template<typename T> inline T mine(vector<T> &vec) { return *min_element(all(vec)); }
+
+struct custom_hash { // https://codeforces.com/blog/entry/62393
+    static uint64_t splitmix64(uint64_t x) { x += 0x9e3779b97f4a7c15; x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9; x = (x ^ (x >> 27)) * 0x94d049bb133111eb; return x ^ (x >> 31); }
+    size_t operator()(uint64_t x) const { static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count(); return splitmix64(x + FIXED_RANDOM); }
+};
+template<typename T, typename U> struct safe_map : public std::unordered_map<T,U,custom_hash> {};
+template<typename T> struct safe_set : public std::unordered_set<T,custom_hash> {};
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) 
+{ os << '{'; string sep; for (const auto &x: v) os << sep << x, sep = ", "; return os << '}';}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) 
+{ return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#ifdef TTL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename T> inline void print(T obj) { cout << obj << '\n'; }
+template<typename T, typename... Args> inline void print(T t, Args... args) { cout << t << " "; print(args...); }
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<int> vi; 
+typedef vector<vector<int>> VVI;
+typedef vector<long long> VLL;
+typedef vector<vector<long long>> VVLL;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<pair<int, int>> VPII;
+typedef vector<vector<pair<int, int>>> VVPII;
+
+VPII dirs{mp(-1,0),mp(1,0),mp(0,-1),mp(0,1)};
+const ld pi = 3.1415926535897932384626433832795;
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+
+const ll inf = -10000000000000000LL;
+const ll AK = 1e9;
+struct Node {
+	Node *l = 0, *r = 0;
+	int lo, hi;
+    ll mset = inf, madd = 0;
+    pll val = {-inf,AK};
+	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
+	Node(vector<ll>& v, int lo, int hi) : lo(lo), hi(hi) {
+		if (lo + 1 < hi) {
+			int mid = lo + (hi - lo)/2;
+			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
+			val = min(l->val, r->val);
+		}
+		else val = {v[lo],lo};
+	}
+	pll query(int L, int R) {
+		if (R <= lo || hi <= L) return {-inf,AK};
+		if (L <= lo && hi <= R) return val;
+		push();
+		return min(l->query(L, R), r->query(L, R));
+	}
+	void set(int L, int R, ll x) {
+		if (R <= lo || hi <= L) return;
+		if (L <= lo && hi <= R) mset = val.F = x, madd = 0, val.S = lo;
+		else {
+			push(), l->set(L, R, x), r->set(L, R, x);
+			val = min(l->val, r->val);
+		}
+	}
+	void add(int L, int R, ll x) {
+		if (R <= lo || hi <= L) return;
+		if (L <= lo && hi <= R) {
+			if (mset != inf) mset += x;
+			else madd += x;
+			val.F += x;
+		}
+		else {
+			push(), l->add(L, R, x), r->add(L, R, x);
+			val = min(l->val, r->val);
+		}
+	}
+	void push() {
+		if (!l) {
+			int mid = lo + (hi - lo)/2;
+			l = new Node(lo, mid); r = new Node(mid, hi);
+		}
+		if (mset != inf)
+			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
+		else if (madd)
+			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
+	}
+};
+
+typedef pair<pii,ll> P;
+void run() {
+    ll n,k,A; cin >> n >> k >> A;
+    vector<vector<P>> pts(k); // bucketed by y
+    rep(i,0,n) {
+        int x,y; ll c; cin >> x >> y >> c;
+        pts[y].pb({{x,y},c});
+    }
+    Node* tree = new Node(0,k+2);
+    rep(i,0,k+2) {
+        tree->set(i,i+1,(k-i)*A);
+    }
+    for (int y = k-1; y>=0; --y) {
+        for (P p : pts[y]) {
+            tree->add(p.F.F+1, k+2, p.S);
+        }
+        int xi = k-y;
+        pll qr = tree->query(0, xi+1);
+        ll handleCost = A*(xi-qr.S) + tree->query(qr.S,qr.S+1).F-A*(k-qr.S);
+        tree->set(xi+1,xi+2, (k-xi-1)*A + handleCost);
+    }
+    print(tree->query(k+1,k+2).F + A);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+    // cout.setf(ios::fixed);
+    // cout.precision(15);
+    // ll t; cin >> t;
+    ll t=1;
+    rep(tests,0,t) run();
+}
